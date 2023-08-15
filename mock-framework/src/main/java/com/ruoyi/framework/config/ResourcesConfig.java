7d4b1b3e93d91d3fdd2a9135c3f3825f3e555838
lancelot <huxi@eptok.com>
2023-08-15 17:55:50 +0800

    github flow

--- mock-framework/src/main/java/com/ruoyi/framework/config/ResourcesConfig.java
+++ mock-framework/src/main/java/com/ruoyi/framework/config/ResourcesConfig.java
@@ -3,13 +3,11 @@ package com.ruoyi.framework.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import com.lancelot.mock.common.config.RuoYiConfig;
 import com.lancelot.mock.common.constant.Constants;
-import com.ruoyi.framework.interceptor.RepeatSubmitInterceptor;
 
 /**
  * 通用配置
@@ -25,8 +23,6 @@ public class ResourcesConfig implements WebMvcConfigurer
     @Value("${shiro.user.indexUrl}")
     private String indexUrl;
 
-    @Autowired
-    private RepeatSubmitInterceptor repeatSubmitInterceptor;
 
     /**
      * 默认首页的设置，当输入域名是可以自动跳转到默认指定的网页
@@ -47,12 +43,4 @@ public class ResourcesConfig implements WebMvcConfigurer
         registry.addResourceHandler("/swagger-ui/**").addResourceLocations("classpath:/META-INF/resources/webjars/springfox-swagger-ui/");
     }
 
-    /**
-     * 自定义拦截规则
-     */
-    @Override
-    public void addInterceptors(InterceptorRegistry registry)
-    {
-        registry.addInterceptor(repeatSubmitInterceptor).addPathPatterns("/**");
-    }
 }
