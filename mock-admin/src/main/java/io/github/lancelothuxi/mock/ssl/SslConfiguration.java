3142948f3f0cd756dfcfae4464a2c5957157a7bc
lancelot <huxi@eptok.com>
2023-09-08 14:02:02 +0800

    https

--- mock-admin/src/main/java/io/github/lancelothuxi/mock/ssl/SslConfiguration.java
+++ mock-admin/src/main/java/io/github/lancelothuxi/mock/ssl/SslConfiguration.java
@@ -1,5 +1,8 @@
 package io.github.lancelothuxi.mock.ssl;
 
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ResourceLoader;
@@ -35,5 +38,23 @@ public class SslConfiguration {
     SSLContext sslContext = SSLContext.getInstance("TLS");
     sslContext.init(keyManagerFactory.getKeyManagers(), null, null);
     return sslContext;
+  }
+
+  @Bean
+  public ServletWebServerFactory servletContainer() {
+    TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+    tomcat.addAdditionalTomcatConnectors(createHTTPConnector());
+    return tomcat;
+
+  }
+
+  private Connector createHTTPConnector() {
+    Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+    //同时启用http（8080）、https（8443）两个端口
+    connector.setScheme("http");
+    connector.setSecure(false);
+    connector.setPort(8555);
+    connector.setRedirectPort(8443);
+    return connector;
   }
 }
