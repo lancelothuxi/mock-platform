66d38798f6bcce9fb590121375255eae5d98a6d0
lancelot <huxi@eptok.com>
2023-09-08 14:59:20 +0800

    add kube_config

---
+++ mock-admin/src/main/java/io/github/lancelothuxi/mock/k8s/webhook/WebHookConfigController.java
@@ -0,0 +1,13 @@
+package io.github.lancelothuxi.mock.k8s.webhook;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author lancelot
+ * @version 1.0
+ * @date 2023/9/8 下午2:23
+ */
+@RestController
+public class WebHookConfigController {
+
+}
