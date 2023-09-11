09847313c43b0fb4e06b0f41fad59e5066181295
lancelot <huxi@eptok.com>
2023-09-11 15:05:56 +0800

    add kube_config

--- mock-admin/src/main/java/io/github/lancelothuxi/mock/Application.java
+++ mock-admin/src/main/java/io/github/lancelothuxi/mock/Application.java
@@ -1,11 +1,8 @@
 package io.github.lancelothuxi.mock;
 
-import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
-import org.apache.dubbo.config.spring.context.annotation.EnableDubboConfig;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * 启动程序
@@ -13,8 +10,6 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
  * @author lancelot huxisuz@gmail.com
  */
 @SpringBootApplication
-@EnableDubbo
-@EnableDubboConfig
 @MapperScan(basePackages = {"io.github.lancelothuxi.mock.mapper","io.github.lancelothuxi.mock.system.mapper"})
 public class Application {
 
