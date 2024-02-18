c0b7b6543e4a53c1a292c11b41ffa3286f792857
lancelothuxi <huxisuz@gmail.com>
2024-02-18 15:57:25 +0800

    fabric docker maven plugin

--- bin/start.sh
+++ bin/start.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+dir=`pwd`
+
 echo "compile and run in docker"
-cd ..
-mvn clean install && cd ./backend/ruoyi-admin && mvn docker:build && mvn docker:run
+cd $dir/..
+mvn clean install && cd $dir/../backend/ruoyi-admin && mvn docker:build && mvn docker:run
+
+
+echo "pnpm install"
+mvn com.github.eirslett:frontend-maven-plugin:1.12.1:npx@install-frontend-dependencies
+
+echo "pnpm dev"
+mvn com.github.eirslett:frontend-maven-plugin:1.12.1:npx@run-dev
