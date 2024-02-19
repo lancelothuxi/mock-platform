aa16eeb4c1617005db3ff0fa12567bd5a643034a
lancelothuxi <huxisuz@gmail.com>
2024-02-19 11:00:17 +0800

    fabric docker maven plugin

--- bin/start.sh
+++ bin/start.sh
@@ -1,11 +1,26 @@
-#!/bin/bash
-dir=`pwd`
+#!/bin/sh
+cd ..
+baseDir=`pwd`
+# 要判断的端口号
+port=8080
+# 检查端口是否被占用
+if netstat -an | grep LISTEN | grep $port > /dev/null; then
+  echo "端口 $port 已被占用"
+  exit 1
+fi
 
 echo "compile and run in docker"
-cd $dir/..
-mvn clean install && cd $dir/../backend/ruoyi-admin && mvn docker:build && mvn docker:run
+mvn clean install
+cd $baseDir/backend/ruoyi-admin
+echo `pwd`
+sh -c "(mvn docker:build && mvn docker:run) &"
 
-cd $dir/../frontend
+# 等待端口被监听
+while ! netstat -an | grep LISTEN | grep $port > /dev/null; do
+  sleep 1
+done
+
+cd $baseDir/frontend
 echo "pnpm install"
 mvn com.github.eirslett:frontend-maven-plugin:1.12.1:npx@install-frontend-dependencies
 
