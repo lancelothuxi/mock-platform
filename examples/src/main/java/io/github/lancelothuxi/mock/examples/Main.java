0ea59a9d65ce3c560a1123e4b945855db441de95
lancelothuxi <huxisuz@gmail.com>
2024-02-23 16:33:18 +0800

    add agent jar in examples

--- examples/src/main/java/io/github/lancelothuxi/mock/examples/Main.java
+++ examples/src/main/java/io/github/lancelothuxi/mock/examples/Main.java
@@ -1,17 +1,14 @@
 package io.github.lancelothuxi.mock.examples;
 
-import feign.Contract;
-import feign.Feign;
-import feign.jackson.JacksonDecoder;
-import feign.jackson.JacksonEncoder;
 import io.github.lancelothuxi.mock.examples.dubbo.apache.DubboApacheExampleService;
-import io.github.lancelothuxi.mock.examples.feign.HelloFeignService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Main {
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        //先等2s
+        Thread.sleep(2000);
         while (true){
            try {
                demoCall();
@@ -29,10 +26,10 @@ public class Main {
     private static void demoCall() {
         // 添加 Mock Agent 参数
         // 创建被 mock 的类的实例
-        ExampleService service = new ExampleService();
+//        ExampleService service = new ExampleService();
         // 调用被 mock 的方法
-        int result = service.add(1, 2);
-        logger.info("cal result = " + result);
+//        int result = service.add(1, 2);
+//        logger.info("cal result = " + result);
 
         DubboApacheExampleService dubboApacheExampleService =new DubboApacheExampleService();
         String sayHelloByDubbo = dubboApacheExampleService.sayHelloByDubbo();
