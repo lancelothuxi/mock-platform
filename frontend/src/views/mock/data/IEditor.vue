0b6558eb09b63727cf3b2c409a03f85d815c247c
lancelot <huxisuz@gmail.com>
2024-04-08 00:17:17 +0800

    update ace editor

--- frontend/src/views/mock/data/IEditor.vue
+++ frontend/src/views/mock/data/IEditor.vue
@@ -1,41 +1,62 @@
 <template>
-  <div ref="ace" class="ace-editor"/>
+    <div class="ace">
+      <p>快速开始-demo</p>
+      <div ref="aceRef" class="ace-editor"></div>
+    </div>
 </template>
 <script setup>
-import ace from 'ace-builds'
-// ace主题包
-import 'ace-builds/src-min-noconflict/theme-kuroir'
-import 'ace-builds/src-min-noconflict/theme-one_dark'
-import 'ace-builds/src-min-noconflict/theme-github'
-// ace语言包
-import 'ace-builds/src-min-noconflict/mode-javascript'
-import 'ace-builds/src-min-noconflict/mode-json5'
-//代码完成
-import 'ace-builds/src-min-noconflict/ext-language_tools'
+import {ref, onMounted, defineProps, watch} from 'vue';
+import ace from 'ace-builds';
+import 'ace-builds/src-min-noconflict/theme-kuroir';
+import 'ace-builds/src-min-noconflict/ext-searchbox';
+import 'ace-builds/src-min-noconflict/mode-javascript';
+import 'ace-builds/src-min-noconflict/ext-language_tools';
 
-const model = defineModel()
-let options = {
-  theme: 'ace/theme/tomorrow', // 主题
-  mode: 'ace/mode/mysql', // 代码匹配模式
-  tabSize: 2, //标签大小
-  fontSize: 14, //设置字号
-  wrap: true, // 用户输入的sql语句，自动换行
-  enableSnippets: true, // 启用代码段
-  showLineNumbers: true, // 显示行号
-  enableLiveAutocompletion: true, // 启用实时自动完成功能 （比如：智能代码提示）
-  enableBasicAutocompletion: true, // 启用基本自动完成功能
-  scrollPastEnd: true, // 滚动位置
-  highlightActiveLine: true, // 高亮当前行
-}
+const aceRef = ref(null);
+const editor = ref(null);
 
-this.editor = ace.edit(this.$refs.ace, this.options);
+const props = defineProps({
+  modelValue: {
+    type: String,
+    default: ''
+  }
+});
 
+onMounted(() => {
+  editor.value = ace.edit(aceRef.value, {
+    useWorker: false,
+    maxLines: 24,
+    minLines: 12,
+    fontSize: 14,
+    theme: 'ace/theme/kuroir',
+    mode: 'ace/mode/javascript',
+    tabSize: 4,
+    readOnly: false
+  });
+
+  editor.value.getSession().setValue(props.modelValue);
+
+});
+
+watch(() => props.modelValue, (newValue) => {
+  if (newValue !== editor.value.getSession().getValue()) {
+    editor.value.getSession().setValue(newValue);
+  }
+});
 </script>
 
 <style scoped>
 .ace-editor {
-  position: static;
-  width: 1000px;
-  height: 200px;
+  margin-bottom: 20px;
+}
+
+.ace-toolbar {
+  display: flex;
+  justify-content: center;
+  margin-top: 20px;
+}
+
+.ace-toolbar > button {
+  margin-left: 20px;
 }
 </style>
